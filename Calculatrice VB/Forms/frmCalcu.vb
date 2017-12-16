01f36cd46c1c37cd48aa319da12a72f205756d9f
William <willi_garn@hotmail.com>
2017-12-16 00:00:00 -0500

    Little bugs on display

--- Calculatrice VB/Forms/frmCalcu.vb
+++ Calculatrice VB/Forms/frmCalcu.vb
@@ -12,15 +12,6 @@
     Private Nombre As String = ""
     Private CalculActuel As String = ""
 
-    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
-        PremierNombre = ""
-        DeuxiemeNombre = ""
-        Resultat = ""
-        Operateur = ""
-        Nombre = ""
-        CalculActuel = ""
-        UpdateText("")
-    End Sub
 
     #Region "Méthodes"
     Private Sub UpdateText(ByVal num As String)
@@ -74,4 +65,94 @@
         End If
     End Sub
 #End Region
+
+    #Region "Events"
+    Private Sub clearButton_Click(sender As Object, e As EventArgs) Handles clearButton.Click
+        PremierNombre = ""
+        DeuxiemeNombre = ""
+        Resultat = ""
+        Operateur = ""
+        Nombre = ""
+        CalculActuel = ""
+        UpdateText("")
+    End Sub
+
+    Private Sub clearEntryButton_Click(sender As Object, e As EventArgs) Handles clearEntryButton.Click
+        Nombre = ""
+        UpdateText("")
+    End Sub
+
+    Private Sub backspaceButton_Click(sender As Object, e As EventArgs) Handles backspaceButton.Click
+        If (Nombre.Length>0) Then
+            Nombre = Nombre.Substring(0,Nombre.Length-1)
+            UpdateText("")
+        End If
+    End Sub
+
+    Private Sub plusButton_Click(sender As Object, e As EventArgs) Handles plusButton.Click
+        UpdateOperator("+")
+    End Sub
+
+    Private Sub minusButton_Click(sender As Object, e As EventArgs) Handles minusButton.Click
+        UpdateOperator("-")
+    End Sub
+
+    Private Sub multiplyButton_Click(sender As Object, e As EventArgs) Handles multiplyButton.Click
+        UpdateOperator("*")
+    End Sub
+
+    Private Sub divideButton_Click(sender As Object, e As EventArgs) Handles divideButton.Click
+        UpdateOperator("/")
+    End Sub
+
+    Private Sub nineButton_Click(sender As Object, e As EventArgs) Handles nineButton.Click
+        UpdateText("9")
+    End Sub
+
+    Private Sub eightButton_Click(sender As Object, e As EventArgs) Handles eightButton.Click
+        UpdateText("8")
+    End Sub
+
+    Private Sub sevenButton_Click(sender As Object, e As EventArgs) Handles sevenButton.Click
+        UpdateText("7")
+    End Sub
+
+    Private Sub sixButton_Click(sender As Object, e As EventArgs) Handles sixButton.Click
+        UpdateText("6")
+    End Sub
+
+    Private Sub fiveButton_Click(sender As Object, e As EventArgs) Handles fiveButton.Click
+        UpdateText("5")
+    End Sub
+
+    Private Sub fourButton_Click(sender As Object, e As EventArgs) Handles fourButton.Click
+        UpdateText("4")
+    End Sub
+
+    Private Sub threeButton_Click(sender As Object, e As EventArgs) Handles threeButton.Click
+        UpdateText("3")
+    End Sub
+
+    Private Sub twoButton_Click(sender As Object, e As EventArgs) Handles twoButton.Click
+        UpdateText("2")
+    End Sub
+
+    Private Sub oneButton_Click(sender As Object, e As EventArgs) Handles oneButton.Click
+        UpdateText("1")
+    End Sub
+
+    Private Sub zeroButton_Click(sender As Object, e As EventArgs) Handles zeroButton.Click
+        UpdateText("0")
+    End Sub
+
+    Private Sub equalButton_Click(sender As Object, e As EventArgs) Handles equalButton.Click
+        Calculer()
+    End Sub
+
+    Private Sub saveHistoryButton_Click(sender As Object, e As EventArgs) Handles saveHistoryButton.Click
+        historique.SauvegarderOperations()
+    End Sub
+
+#End Region
+
 End Class
